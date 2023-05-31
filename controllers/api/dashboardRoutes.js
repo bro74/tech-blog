8e7c08a489c160edb4e7aaba4f16746fcb834e92
Bro74 <bmwhite1014@gmail.com>
2023-05-31 11:52:13 -0400

    commentRoutes post logic

--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -3,5 +3,23 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
+router.post('/', withAuth, (req, res) => {
+    console.log(req.body)
+    console.log(req.session)
+  // check the session
+  if (req.session) {
+    Comment.create({
+      content: req.body.content,
+      post_id: req.body.post_id,
+      // use the id from the session
+      user_id: req.session.user_id,
+    })
+      .then(dbCommentData => res.redirect(`/post/${req.body.post_id}`))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+});
 
 module.exports = router;
